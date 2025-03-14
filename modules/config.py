9ab104045f90ed0d7b3f137e89a47636da367418
n1lby73 <n1lby73@gmail.com>
2025-03-14 16:41:14 +0100

    removed flask setup

--- modules/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from modules import app
-import os
-
-load_dotenv()
-
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
